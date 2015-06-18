618f94f998996146a05949ddca63398faec7779c
Vasya Redneck <vasya.redneck@gmail.com>
2015-06-18 16:26:46 +0300

    plot1

--- plot1.R
+++ plot1.R
@@ -1,13 +1,24 @@
 library("data.table")
 
 if (!exists("NEI")) {
-    NEI <- readRDS("summarySCC_PM25.rds")
+    NEI <- data.table(readRDS("summarySCC_PM25.rds"))
 }
 if (!exists("SCC")) {
-    SCC <- readRDS("Source_Classification_Code.rds")
+    SCC <- data.table(readRDS("Source_Classification_Code.rds"))
 }
 
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources
+# for each of the years 1999, 2002, 2005, and 2008.
+total.emissions <- NEI[, sum(Emissions), by = year]
+total.emissions[, Total := V1]
+    
+with(total.emissions, {
+    print(as.character(year))
+    plot(Total ~ year, main = "Total Emissions from PM2.5")
+    axis(1, at = year, labels = as.character(year))
+    abline(lm(Total ~ year), col = "red")
+})
 
-
-d <- dev.copy(png, filename = "plot1.png", width = 480, height = 480)
+d <- dev.copy(png, filename = "plot1.png", width = 640, height = 480)
 dev.off(d)
