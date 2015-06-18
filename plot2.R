2c55a6b188f0eb40ddc29000eb8101335a930cf4
Vasya Redneck <vasya.redneck@gmail.com>
2015-06-18 13:32:05 +0300

    initial mock

---
+++ plot2.R
@@ -0,0 +1,13 @@
+library("data.table")
+
+if (!exists("NEI")) {
+    NEI <- readRDS("summarySCC_PM25.rds")
+}
+if (!exists("SCC")) {
+    SCC <- readRDS("Source_Classification_Code.rds")
+}
+
+
+
+d <- dev.copy(png, filename = "plot2.png", width = 480, height = 480)
+dev.off(d)
